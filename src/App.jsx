25552cabbaf4932b213e11d7538b1e461c264699
飞刀和雨 <wangwenzhou@hotmail.com>
2025-10-28 01:34:32 +0800

    box

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Hero from './components/Hero.jsx';
 import NavBar from './components/NavBar.jsx';
+import ProductViewer from './components/ProductView.jsx';
 
 const App = () => {
   return (
     <main>
       <NavBar />
       <Hero />
+      <ProductViewer />
     </main>
   )
 };
